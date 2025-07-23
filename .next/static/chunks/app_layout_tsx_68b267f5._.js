a83a51d7d7a60ea4b3c0bf4fe95ae589a38ae661
Kyle <86751427+kyleb5@users.noreply.github.com>
2025-07-22 22:24:34 -0500

    Delete .next directory

--- .next/static/chunks/app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules_66870cfd._.js",
-  "static/chunks/_6a6da0f8._.js"
-],
-    source: "dynamic"
-});
