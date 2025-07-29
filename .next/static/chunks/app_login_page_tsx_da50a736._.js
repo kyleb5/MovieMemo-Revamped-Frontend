fef8fecfae02f1e147b11f0e894d0a0eb648342b
Kyle <86751427+kyleb5@users.noreply.github.com>
2025-07-28 22:04:13 -0500

    Delete .next directory

--- .next/static/chunks/app_login_page_tsx_da50a736._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f54fd68f._.js"
-],
-    source: "dynamic"
-});
